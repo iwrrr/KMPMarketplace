1b03923157aa013acf707d842893279466c02af9
Irya <iryadev15@gmail.com>
2023-12-13 02:24:02 +0700

    refactor: rename function get product list

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductApi.kt
+++ apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductApi.kt
@@ -1,21 +1,27 @@
 package com.kmp.api.product
 
 import com.kmp.api.product.model.ProductList
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class ProductApi {
 
-    fun getProducts(): List<ProductList> {
-        return listOf(
-            ProductList(
-                id = 2847,
-                name = "Meja",
-                price = 78.832
-            ),
-            ProductList(
-                id = 2847,
-                name = "Kursi",
-                price = 78.832
-            ),
-        )
+    fun getProductList(): Flow<List<ProductList>> {
+        return flow {
+            emit(
+                listOf(
+                    ProductList(
+                        id = 2847,
+                        name = "Meja",
+                        price = 78.832
+                    ),
+                    ProductList(
+                        id = 2847,
+                        name = "Kursi",
+                        price = 78.832
+                    ),
+                )
+            )
+        }
     }
 }

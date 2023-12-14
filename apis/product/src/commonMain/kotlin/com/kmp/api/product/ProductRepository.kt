d14368cb58283982f567e7e04d08e5e92df77760
Irya <iryadev15@gmail.com>
2023-12-14 22:58:41 +0700

    feat: implement repository for get product list

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductRepository.kt
+++ apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductRepository.kt
@@ -1,27 +1,24 @@
 package com.kmp.api.product
 
+import com.kmp.api.product.model.Mapper
 import com.kmp.api.product.model.ProductList
+import com.kmp.api.product.model.ProductListResponse
+import io.ktor.client.call.body
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class ProductRepository {
 
-    fun getProductList(): Flow<List<ProductList>> {
+    private val dataSources by lazy { ProductDataSources() }
+
+    suspend fun getProductList(): Flow<List<ProductList>> {
+        val data = dataSources
+            .getProductList()
+            .body<ProductListResponse>()
+            .let { Mapper.mapResponseToList(it) }
+
         return flow {
-            emit(
-                listOf(
-                    ProductList(
-                        id = 2847,
-                        name = "Meja",
-                        price = 78.832
-                    ),
-                    ProductList(
-                        id = 2847,
-                        name = "Kursi",
-                        price = 78.832
-                    ),
-                )
-            )
+            emit(data)
         }
     }
 }

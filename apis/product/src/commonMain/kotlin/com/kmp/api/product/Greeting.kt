33297803fa4fadbc6002939472a29f16ff8aa00e
Irya <iryadev15@gmail.com>
2023-12-18 02:01:10 +0700

    feat: add category list response model

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.kmp.api.product
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- apis/product/src/commonMain/kotlin/com/kmp/api/product/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kmp.api.product
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform

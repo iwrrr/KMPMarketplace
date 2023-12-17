4ebc893c448b8414cafa09ee040dafa90c56e913
Irya <iryadev15@gmail.com>
2023-12-17 12:38:40 +0700

    feat: implement navigation with precompose

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -6,18 +6,33 @@ import com.kmp.features.home.Home
 import com.kmp.libraries.core.LocalAppConfig
 import com.kmp.libraries.core.viewmodel.LocalViewModelHost
 import com.kmp.libraries.core.viewmodel.ViewModelHost
+import moe.tlaster.precompose.PreComposeApp
+import moe.tlaster.precompose.navigation.NavHost
+import moe.tlaster.precompose.navigation.rememberNavigator
+import moe.tlaster.precompose.navigation.transition.NavTransition
 
 @Composable
 fun App() {
     val viewModelHost = remember { ViewModelHost() }
     val appConfigProvider = remember { AppConfigProvider() }
 
-    CompositionLocalProvider(
-        LocalViewModelHost provides viewModelHost,
-        LocalAppConfig provides appConfigProvider
-    ) {
-        MaterialTheme {
-            Home()
+    PreComposeApp {
+        CompositionLocalProvider(
+            LocalViewModelHost provides viewModelHost,
+            LocalAppConfig provides appConfigProvider
+        ) {
+            MaterialTheme {
+                val navigator = rememberNavigator()
+                NavHost(
+                    navigator = navigator,
+                    navTransition = NavTransition(),
+                    initialRoute = "/home"
+                ) {
+                    scene(route = "/home") {
+                        Home()
+                    }
+                }
+            }
         }
     }
 }

a95047b5025f43a64259b7b5a94b5fade981e5dd
Irya <iryadev15@gmail.com>
2023-12-17 09:15:51 +0700

    feat: implement app config

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductDataSources.kt
+++ apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductDataSources.kt
@@ -1,9 +1,12 @@
 package com.kmp.api.product
 
+import com.kmp.libraries.core.AppConfig
 import com.kmp.libraries.core.network.NetworkDataSources
 import io.ktor.client.statement.HttpResponse
 
-class ProductDataSources : NetworkDataSources("https://marketfake.fly.dev/") {
+class ProductDataSources(
+    appConfig: AppConfig
+) : NetworkDataSources(appConfig.baseUrl) {
 
     suspend fun getProductList(): HttpResponse {
         val endpoint = "product"

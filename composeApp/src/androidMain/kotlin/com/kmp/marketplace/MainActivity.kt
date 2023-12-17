433e2e25d032b99e1d190024cb2ccc99b7191a88
Irya <iryadev15@gmail.com>
2023-12-18 01:59:13 +0700

    feat: enable edge to edge in android

--- composeApp/src/androidMain/kotlin/com/kmp/marketplace/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kmp/marketplace/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.kmp.marketplace
 
 import App
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -12,7 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
 
         setContent {
             App()

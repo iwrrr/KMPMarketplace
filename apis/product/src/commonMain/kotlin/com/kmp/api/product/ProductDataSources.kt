bc1d6ec59727852636f03f0340285838e2647a92
Irya <iryadev15@gmail.com>
2023-12-18 02:02:42 +0700

    feat: add category list data source

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductDataSources.kt
+++ apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductDataSources.kt
@@ -8,6 +8,11 @@ class ProductDataSources(
     appConfig: AppConfig
 ) : NetworkDataSources(appConfig.baseUrl) {
 
+    suspend fun getCategoryList(): HttpResponse {
+        val endpoint = "product/category"
+        return getHttpResponse(endpoint)
+    }
+
     suspend fun getProductList(): HttpResponse {
         val endpoint = "product"
         return getHttpResponse(endpoint)

680cc1444410da82f725bacb3ec7c05ce935bb85
Irya <iryadev15@gmail.com>
2023-12-18 02:03:06 +0700

    feat: implement repository for get category list

--- apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductRepository.kt
+++ apis/product/src/commonMain/kotlin/com/kmp/api/product/ProductRepository.kt
@@ -1,8 +1,10 @@
 package com.kmp.api.product
 
 import com.kmp.api.product.model.Mapper
-import com.kmp.api.product.model.ProductList
-import com.kmp.api.product.model.ProductListResponse
+import com.kmp.api.product.model.category.Category
+import com.kmp.api.product.model.category.CategoryResponse
+import com.kmp.api.product.model.product.Product
+import com.kmp.api.product.model.product.ProductResponse
 import com.kmp.libraries.core.AppConfig
 import com.kmp.libraries.core.repository.Repository
 import com.kmp.libraries.core.state.Async
@@ -14,18 +16,35 @@ class ProductRepository(
 
     private val dataSources by lazy { ProductDataSources(appConfig) }
 
-    fun getProductList(): Flow<Async<List<ProductList>>> {
+    fun getCategoryList(): Flow<Async<List<Category>>> {
+        return suspend {
+            dataSources.getCategoryList()
+        }.reduce<CategoryResponse, List<Category>> { response ->
+            val responseData = response.data
 
+            if (responseData.isNullOrEmpty()) {
+                val throwable = Throwable("Category is empty")
+                Async.Failure(throwable)
+            } else {
+                val moreCategory = Category(id = -1, name = "More", description = "")
+                val data = Mapper.mapResponseToCategoryList(response).take(5).toMutableList()
+                data.add(moreCategory)
+                Async.Success(data)
+            }
+        }
+    }
+
+    fun getProductList(): Flow<Async<List<Product>>> {
         return suspend {
             dataSources.getProductList()
-        }.reduce<ProductListResponse, List<ProductList>> { response ->
+        }.reduce<ProductResponse, List<Product>> { response ->
             val responseData = response.data
 
             if (responseData.isNullOrEmpty()) {
                 val throwable = Throwable("Product is empty")
                 Async.Failure(throwable)
             } else {
-                val data = Mapper.mapResponseToList(response)
+                val data = Mapper.mapResponseToProductList(response)
                 Async.Success(data)
             }
         }

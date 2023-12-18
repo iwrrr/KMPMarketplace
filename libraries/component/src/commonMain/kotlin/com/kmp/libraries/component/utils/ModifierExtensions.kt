8658ffd6f232bb5d5794ce79f5105290a0c7e565
Irya <iryadev15@gmail.com>
2023-12-19 01:02:27 +0700

    feat: add fullOffset modifier for ignore content padding

--- libraries/component/src/commonMain/kotlin/com/kmp/libraries/component/utils/ModifierExtensions.kt
+++ libraries/component/src/commonMain/kotlin/com/kmp/libraries/component/utils/ModifierExtensions.kt
@@ -1,6 +1,8 @@
 package com.kmp.libraries.component.utils
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
@@ -9,37 +11,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.offset
 
-//fun Modifier.bounceClickable(
-//    enabled: Boolean = true,
-//    onClick: () -> Unit
-//) = composed {
-//    val interactionSource = remember { MutableInteractionSource() }
-//    val isPressed by interactionSource.collectIsPressedAsState()
-//
-//    val animationTransition = updateTransition(isPressed, label = "BouncingClickableTransition")
-//    val scaleFactor by animationTransition.animateFloat(
-//        targetValueByState = { pressed -> if (pressed) 0.95f else 1f },
-//        label = "BouncingClickableScaleFactorTransition",
-//    )
-//    val opacity by animationTransition.animateFloat(
-//        targetValueByState = { pressed -> if (pressed) 0.7f else 1f },
-//        label = "BouncingClickableOpacityTransition"
-//    )
-//
-//    this
-//        .graphicsLayer {
-//            this.scaleX = scaleFactor
-//            this.scaleY = scaleFactor
-//            this.alpha = opacity
-//        }
-//        .clickable(
-//            interactionSource = interactionSource,
-//            indication = null,
-//            enabled = enabled,
-//            onClick = onClick
-//        )
-//}
+fun Modifier.fullOffset(offset: Int) = composed {
+    val density = LocalDensity.current
+    val offsetPx = with(density) {
+        offset.dp.roundToPx()
+    }
+
+    Modifier.layout { measurable, constraints ->
+        val looseConstraints = constraints.offset(offsetPx * 2, 0)
+        val placeable = measurable.measure(looseConstraints)
+        layout(placeable.width, placeable.height) {
+            placeable.placeRelative(0, 0)
+        }
+    }
+}
 
 fun Modifier.bounceClickable(
     scaleDown: Float = 0.95f,
@@ -52,12 +42,24 @@ fun Modifier.bounceClickable(
         Animatable(1f)
     }
 
+    val animationSpec = SpringSpec<Float>(
+        dampingRatio = Spring.DampingRatioMediumBouncy,
+        stiffness = Spring.StiffnessMediumLow,
+    )
+
     LaunchedEffect(interactionSource) {
         interactionSource.interactions.collect { interaction ->
             when (interaction) {
                 is PressInteraction.Press -> animatable.animateTo(scaleDown)
-                is PressInteraction.Release -> animatable.animateTo(1f)
-                is PressInteraction.Cancel -> animatable.animateTo(1f)
+                is PressInteraction.Release -> animatable.animateTo(
+                    targetValue = 1f,
+                    animationSpec = animationSpec
+                )
+
+                is PressInteraction.Cancel -> animatable.animateTo(
+                    targetValue = 1f,
+                    animationSpec = animationSpec
+                )
             }
         }
     }
